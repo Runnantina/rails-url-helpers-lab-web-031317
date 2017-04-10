c32154d6aecac1719e7e75f7039c07476b917f6f
Runnan Zhong <r.tina.zhong@gmail.com>
2017-04-10 18:35:53 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,34 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+      # if student is active then string would be "This student is currently active."
+      # else it should return "This student is currently inactive."
+      if @student.active == true
+        @status = "This student is currently active."
+      else
+        @status = "This student is currently inactive."
+      end
+
   end
+
+  def activate
+    @student = Student.find(params[:id])
+      if @student.active == true
+        @student.active = false
+      else @student.active == false
+        @student.active = true
+      end
+      @student.save
+      redirect_to student_path(@student)
+  end
+
+
 
   private
 
